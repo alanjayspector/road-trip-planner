3bc5c66228c9f60149b043dc34073c080844d77e
Alan Spector <alanjayspector@icloud.com>
2025-09-06 14:37:40 -0700

    added firebase linkage

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAyzNwouCNFaSWOl-jSeZneGZ_7LvMRVD4",
+  authDomain: "road-trip-planner-23d11.firebaseapp.com",
+  projectId: "road-trip-planner-23d11",
+  storageBucket: "road-trip-planner-23d11.firebasestorage.app",
+  messagingSenderId: "965276388168",
+  appId: "1:965276388168:web:f05c8393ecaef74a70c17d",
+  measurementId: "G-DRJNZKPBFM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
